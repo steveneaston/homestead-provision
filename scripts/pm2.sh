e469fcf5c11c3e8211a3a393870d3d22dd923df5
Steven Easton <steve.easton@gmail.com>
2015-10-13 12:13:36 +0100

    Use mtab to check mount point has been mounted

--- scripts/pm2.sh
+++ scripts/pm2.sh
@@ -14,17 +14,18 @@ fi
 if [ ! -f /etc/init/workaround-vagrant-bug-6074.conf ]; then
     echo ">>> Installing PM2 Workaround"
 
-    workaround="# workaround for https://github.com/mitchellh/vagrant/issues/6074
-    start on filesystem
-    task
+workaround="# workaround for https://github.com/mitchellh/vagrant/issues/6074
+start on filesystem
+task
 
-    env MOUNTPOINT=/vagrant
+env MOUNTPOINT=/var/www
 
-    script
-      until mountpoint -q \$MOUNTPOINT; do sleep 1; done
-      /sbin/initctl emit --no-wait vagrant-mounted MOUNTPOINT=\$MOUNTPOINT
-    end script
-    "
+script
+#  until mountpoint -q \$MOUNTPOINT; do sleep 1; done
+   until (cat /etc/mtab | grep \$MOUNTPOINT); do sleep 1; done
+  /sbin/initctl emit --no-wait vagrant-mounted MOUNTPOINT=\$MOUNTPOINT
+end script
+"
 
     echo "$workaround" > "/etc/init/workaround-vagrant-bug-6074.conf"
 fi
@@ -32,21 +33,21 @@ fi
 if [ ! -f /etc/init/pm2-upstart.conf ]; then
     echo ">>> Installing PM2 Upstart Script"
 
-    upstart="description \"Start pm2 on vagrant-mounted upstart event\"
-    author      \"Me\"
+upstart="description \"Start pm2 on vagrant-mounted upstart event\"
+author      \"Me\"
 
-    start on vagrant-mounted
+start on vagrant-mounted
 
-    expect fork
+expect fork
 
-    setgid vagrant
-    setuid vagrant
+setgid vagrant
+setuid vagrant
 
-    script
-     export HOME=/home/vagrant
-     sudo /usr/bin/pm2 resurrect
-    end script
-    "
+script
+ export HOME=/home/vagrant
+ sudo /usr/bin/pm2 resurrect
+end script
+"
 
     echo "$upstart" > "/etc/init/pm2-upstart.conf"
 fi
